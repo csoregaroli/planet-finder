a557d8d8fad87f71b7256f114b63102e67e5195b
Cameron Soregaroli <csoregaroli@gmail.com>
2022-11-27 20:37:21 -0500

    finished project

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ const fs = require('fs')
 const habitablePlanets = []
 
 function isHabitablePlanet(planet) {
-  return planet['koi_disposition'] === 'CONFIRMED'
+  const planetIsConfirmed = planet['koi_disposition'] === 'CONFIRMED'
+  const planetExceedsMinLight = planet['koi_insol'] > 0.36
+  const planetUnderMaxLight = planet['koi_insol'] < 1.11
+  const planetIsSmallEnough = planet['koi_prad'] < 1.6
+
+  return (
+    planetIsConfirmed &&
+    planetExceedsMinLight &&
+    planetUnderMaxLight &&
+    planetIsSmallEnough
+  )
 }
 
 fs.createReadStream('kepler_data.csv')
@@ -23,6 +33,11 @@ fs.createReadStream('kepler_data.csv')
     console.log(err)
   })
   .on('end', () => {
-    console.log(habitablePlanets)
-    console.log('done')
+    console.log(
+      habitablePlanets.map((planet) => {
+        const planetName = planet['kepler_name']
+        return planetName
+      })
+    )
+    console.log(`${habitablePlanets.length} habitable planets found!`)
   })
