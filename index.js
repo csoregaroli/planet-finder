a282034d94c7c41c89c99dc5e3f949025d8494ba
Cameron Soregaroli <csoregaroli@gmail.com>
2022-11-27 19:44:58 -0500

    initialized project, set up csv parsing function

---
+++ index.js
@@ -0,0 +1,28 @@
+const { parse } = require('csv-parse')
+const fs = require('fs')
+
+const habitablePlanets = []
+
+function isHabitablePlanet(planet) {
+  return planet['koi_disposition'] === 'CONFIRMED'
+}
+
+fs.createReadStream('kepler_data.csv')
+  .pipe(
+    parse({
+      comment: '#',
+      columns: true,
+    })
+  )
+  .on('data', (data) => {
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data)
+    }
+  })
+  .on('error', (err) => {
+    console.log(err)
+  })
+  .on('end', () => {
+    console.log(habitablePlanets)
+    console.log('done')
+  })
